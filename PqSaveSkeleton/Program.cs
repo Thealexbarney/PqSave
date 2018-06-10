4414cd38c7e7babf854d87a96b0bc20207ded533
Alex Barney <thealexbarney@gmail.com>
2018-06-09 22:33:50 -0500

    Create skeleton project

---
+++ PqSaveSkeleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace PqSaveSkeleton
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte[] savefile = File.ReadAllBytes("user");
+            SaveManager save = new SaveManager(savefile);
+
+            // Work with save object
+
+            byte[] savefileNew = save.Export();
+            File.WriteAllBytes("user_new", savefileNew);
+        }
+    }
+}
---
+++ PqSaveSkeleton/README.md
@@ -0,0 +1,2 @@
+This is an example project for reading and writing the save file.
+Everything needed for reading and writing the save has been placed in `SaveManager.cs`
